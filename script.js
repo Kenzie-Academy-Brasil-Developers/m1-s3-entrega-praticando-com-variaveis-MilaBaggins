98b8f705ff3c68d4f853c7906eb644e61fcfa50a
camila de souza <camila.sparquitetura@gmail.com>
2022-08-08 11:13:07 -0300

    Primeira entrega

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Leia"
+let sobrenome = " Skywalker"
+let anoNascimento = 1999
+let anoATual = 2022
+let idade = anoATual - anoNascimento
+let cidade = "Naboo"
+
+console.log(`Eu sou a ${nome + sobrenome}, tenho ${idade} anos e moro em ${cidade}.`)
